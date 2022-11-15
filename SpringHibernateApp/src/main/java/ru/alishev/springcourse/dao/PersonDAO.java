cd2d32e3162607ce92213fc7a65667cc7049f1f9
Aleksey Nechunaev <aleksey.nechunaev@gmail.com>
2022-11-15 21:53:50 +0700

    add crud methods on PersonDao, fix templates

--- SpringHibernateApp/src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ SpringHibernateApp/src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -26,25 +26,34 @@ public class PersonDAO {
     public List<Person> index() {
         Session session = sessionFactory.getCurrentSession();
 
-        List<Person> people = session.createQuery("select p from Person p", Person.class)
+        return session.createQuery("select p from Person p", Person.class)
                 .getResultList();
-
-        return people;
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
     }
 
+    @Transactional
     public void save(Person person) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Person person = show(id);
+        person.setName(updatedPerson.getName());
+        person.setAge(updatedPerson.getAge());
+        session.save(person);
     }
 
+    @Transactional
     public void delete(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(show(id));
     }
 }
